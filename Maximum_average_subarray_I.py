2872c47ed6953cfe9848e391830b89889beae23c
WhateverOP <oskin.pavel.v@gmail.com>
2024-02-21 16:42:07 +0300

    Maximum_average_subarray_I is added

--- Maximum_average_subarray_I.py
+++ Maximum_average_subarray_I.py
@@ -5,3 +5,23 @@ Find a contiguous subarray whose length is equal to k that has the maximum avera
 Any answer with a calculation error less than 10-5 will be accepted.
 '''
 
+def get_max_average(nums, k):
+    l = 0
+    r = k - 1
+    now_sum = sum(nums[l:r+1])
+    max_sum = now_sum
+    while r < len(nums) - 1:
+        r += 1
+        l += 1
+        now_sum = now_sum - nums[l - 1] + nums[r]
+        if now_sum > max_sum:
+            max_sum = now_sum
+    return max_sum / k
+
+nums = [1,12,-5,-6,50,3]
+k = 4
+# nums = [5]
+# k = 1
+ans = get_max_average(nums, k)
+print(ans)
+# Output: 12.75000
